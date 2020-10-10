43d728395c44a8882e25e5964ada9da08b07c2a3
yangdaqiang <cloudyoeh@gmail.com>
2020-10-10 17:38:26 +0800

    add some function

--- visual.R
+++ visual.R
@@ -2,7 +2,18 @@
 text <- read.csv("D:\\CloudStation-ZS\\R\\guizhou-real-assets-scraping\\anshun_asset2020-10-10.csv", 
                  header = T, encoding = 'UTF-8')
 
-  Encoding(text) <- "UTF-8"
 
-ggplot(data = project_t) + geom_point(mapping = aes(x = ceti_no, y = average_price)， color = ) 
+
+ggplot(text, aes(x = presale, y = average_price, color = developer)) + geom_point()
+
+text_small <- text %>% filter(average_price > 0, presale < 500)
+  
+ggplot(text_small, aes(x = presale, y = average_price, color = developer)) + geom_point()
+
+ggplot(text_small, aes(x = presale, y = average_price)) + geom_point() + facet_wrap(~developer)
+
+
+  
+
+ggplot(data = text) + geom_point(mapping = aes(x = presale, y = average_price), color = developer) 
 
